39acad564fa9a8e7ce093d924617a7d516160be9
Hu Gezhi <hugezhi23@mails.ucas.ac.cn>
2026-01-13 18:41:02 +0800

    finish O0

---
+++ asmgen/src/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <bits/stdc++.h>
+using namespace std;
+
+extern int            optimize_level;
+extern vector<string> ir_lines;
+extern vector<string> asm_lines;
+
+extern void asmgen();
+
+#endif // MAIN_H
